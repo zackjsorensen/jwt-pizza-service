01a89f7c5d377682a240b5f77a19bf0bea9b2f32
zackjsorensen <zackjsorensen@gmail.com>
2026-02-02 22:54:52 -0700

    test 'add menu item success' passes

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
 const app = require('../service');
-const { login } = require('./testUtils');
+const { createAdminUser, registerAdminUser } = require('./testUtils');
+const { DB, Role } = require('../database/database.js');
+
+beforeAll(async () => {
+    await createAdminUser();
+});
 
 test('get menu', async () => {
 
@@ -18,6 +23,43 @@ test('get menu', async () => {
       price: expect.any(Number),
     })
   ]));
- 
 }); 
+
+test('add menu item unauthorized', async () => {
+
+  const newMenuItem = { title: 'Student', description: 'No topping, no sauce, just carbs', image: 'pizza9.png', price: 0.0001 };
+
+  const orderRes = await request(app)
+    .put('/api/order/menu')
+    .send(newMenuItem);
+
+  expect(orderRes.status).toBe(401);
+  expect(orderRes.body.message).toBe('unauthorized');
+});
+
+test('add menu item success', async () => {
+  let adminRes = await registerAdminUser();
+  expect(adminRes.status).toBe(200);
+  let token = adminRes.body.token;
+  expect(token).toBeDefined();
+
+  const newMenuItem = { title: 'Student', description: 'No topping, no sauce, just carbs', image: 'pizza9.png', price: 0.0001 };
+  
+  const orderRes = await request(app)
+    .put('/api/order/menu')
+    .set('Authorization', `Bearer ${token}`)
+    .send(newMenuItem);
+
+  expect(orderRes.status).toBe(200);
+  expect(orderRes.body).toEqual(expect.arrayContaining([
+    expect.objectContaining({
+      id: expect.any(Number),
+      title: 'Student',
+      image: 'pizza9.png',
+      description: 'No topping, no sauce, just carbs',
+      price: 0.0001,
+    })
+  ]));
+
+});
 
--- src/routes/testUtils.js
+++ src/routes/testUtils.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../service");
+const { DB, Role } = require('../database/database.js');
 
 // register a user to use for test, get user back with auth token
 const startSession = async () => {
@@ -10,4 +11,31 @@ const startSession = async () => {
     return testUser;
 };
 
-module.exports = { startSession };
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+// register admin user for test
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
+async function registerAdminUser() {
+  const admin = await createAdminUser();
+  const adminLoginRes = await request(app)
+    .put('/api/auth')
+    .send({ email: admin.email, password: admin.password });
+  return adminLoginRes;
+}
+
+async function getActiveAdminUser(){
+  const adminToken = await registerAdminUserAndGetToken();
+  
+}
+
+module.exports = { startSession, randomName, createAdminUser, registerAdminUser };
