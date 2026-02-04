1f79868bdd31616aa7f2491dcecd3e225c1426ba
zackjsorensen <zackjsorensen@gmail.com>
2026-02-04 14:21:31 -0700

    Adding statemetns to before Alls to populate database with test data

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -2,8 +2,18 @@ const request = require('supertest');
 const app = require('../service');
 const { createAdminUser, registerAdminUser, startSession } = require('./testUtils');
 
+let adminRes;
+
 beforeAll(async () => {
     await createAdminUser();
+    adminRes = await registerAdminUser();
+    expect(adminRes.status).toBe(200);
+    let menuRes = await request(app)
+      .put('/api/order/menu')
+      .set('Authorization', `Bearer ${adminRes.body.token}`)
+      .send({ title: 'Veggie', description: 'Loaded with veggies', image: 'pizza1.png', price: 0.05 });
+
+    expect(menuRes.status).toBe(200);
 });
 
 test('get menu', async () => {
@@ -15,16 +25,16 @@ test('get menu', async () => {
   expect(orderRes.body).toBeDefined();
   expect(Array.isArray(orderRes.body)).toBe(true);
 
-  // expect(orderRes.body.length).toBeGreaterThan(0);
-  // expect(orderRes.body).toEqual(expect.arrayContaining([
-  //   expect.objectContaining({
-  //     id: expect.any(Number),
-  //     title: expect.any(String),
-  //     image: expect.any(String),
-  //     description: expect.any(String),
-  //     price: expect.any(Number),
-  //   })
-  // ]));
+  expect(orderRes.body.length).toBeGreaterThan(0);
+  expect(orderRes.body).toEqual(expect.arrayContaining([
+    expect.objectContaining({
+      id: expect.any(Number),
+      title: expect.any(String),
+      image: expect.any(String),
+      description: expect.any(String),
+      price: expect.any(Number),
+    })
+  ]));
 }); 
 
 test('add menu item unauthorized', async () => {
@@ -40,7 +50,7 @@ test('add menu item unauthorized', async () => {
 });
 
 test('add menu item success', async () => {
-  let adminRes = await registerAdminUser();
+  // let adminRes = await registerAdminUser();
   expect(adminRes.status).toBe(200);
   let token = adminRes.body.token;
   expect(token).toBeDefined();
