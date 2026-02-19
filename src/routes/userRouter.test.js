f586adc3e27fe6f9fe9bb8b8334659f30610c9e5
zackjsorensen <zackjsorensen@gmail.com>
2026-02-19 15:58:21 -0700

    Cleaning lint

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -31,7 +31,7 @@ test("list users unauthorized", async () => {
 
 
 test('list users', async () => {
-  const [user, userToken] = await registerUser(request(app));
+  const [, userToken] = await registerUser(request(app));
   const listUsersRes = await request(app)
     .get('/api/user')
     .set('Authorization', 'Bearer ' + userToken);
