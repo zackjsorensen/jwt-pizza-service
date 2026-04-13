ab8ff6b1694afa66b9427cb49f5c380fd5cea376
zackjsorensen <zackjsorensen@gmail.com>
2026-04-12 21:50:36 -0600

    Adding authorization required for delete franchise

--- src/routes/franchiseRouter.js
+++ src/routes/franchiseRouter.js
@@ -96,7 +96,12 @@ franchiseRouter.post(
 // deleteFranchise
 franchiseRouter.delete(
   '/:franchiseId',
+  authRouter.authenticateToken,
   asyncHandler(async (req, res) => {
+    if (!req.user.isRole(Role.Admin)) {
+      throw new StatusCodeError('unable to delete a franchise', 403);
+    }
+
     const franchiseId = Number(req.params.franchiseId);
     await DB.deleteFranchise(franchiseId);
     res.json({ message: 'franchise deleted' });
